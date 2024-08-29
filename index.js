3405c77d316e2761e9bd6d43613a137f300b6c1c
Dillon <81748327+Wongsodillon@users.noreply.github.com>
2024-08-29 16:45:37 +0700

    merge pls

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ import dotenv from "dotenv";
 import db from "./Database.js";
 import Route from "./Route.js";
 
-// import Users from "./model/Users.js";
-// import Companies from "./model/Companies.js";
-// import JobMatches from "./model/JobMatches.js";
-// import Jobs from "./model/Jobs.js";
-// import JobSkills from "./model/JobSkills.js";
-// import MatchedSkills from "./model/MatchedSkills.js";
-// import Skills from "./model/Skills.js";
-// import UsersExperience from "./model/UsersExperience.js";
-// import UserSkills from "./model/UserSkills.js";
-// import UserTitles from "./model/UserTitles.js";
-// import SavedJobs from "./model/SavedJobs.js";
+import Users from "./model/Users.js";
+import Companies from "./model/Companies.js";
+import JobMatches from "./model/JobMatches.js";
+import Jobs from "./model/Jobs.js";
+import JobSkills from "./model/JobSkills.js";
+import MatchedSkills from "./model/MatchedSkills.js";
+import Skills from "./model/Skills.js";
+import UsersExperience from "./model/UsersExperience.js";
+import UserSkills from "./model/UserSkills.js";
+import UserTitles from "./model/UserTitles.js";
+import SavedJobs from "./model/SavedJobs.js";
 
 dotenv.config();
 const app = express();
