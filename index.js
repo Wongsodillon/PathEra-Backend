b6097f1787c5f4cf0ccf5af49acb2625b30f2d6b
albrtleonardi <leoneeroooo@gmail.com>
2024-09-06 14:06:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -152,6 +152,32 @@ process.on("exit", () => {
   db.close();
 });
 
+const job_skills = [];
+
+fs.createReadStream("../dataset/job_skills.csv")
+  .pipe(csv())
+  .on("data", (data) => {
+    job_skills.push({
+      job_id: parseInt(data.job_id),
+      skill_id: parseInt(data.skill_id),
+    });
+  })
+  .on("end", async () => {
+    try {
+      await db.sync();
+
+      const inserted = await JobSkills.bulkCreate(job_skills, {
+        ignoreDuplicates: true,
+      });
+
+      console.log(
+        `JobSkills data has been successfully seeded with ${inserted.length} entries.`
+      );
+    } catch (error) {
+      console.error("Error seeding job_skills data:", error);
+    }
+  });
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
