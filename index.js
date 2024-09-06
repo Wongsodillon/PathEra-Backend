1c9fd8635b5af550e65d3a77ec3e66e417f70555
albrtleonardi <leoneeroooo@gmail.com>
2024-09-06 14:06:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ import AnswerKey from "./model/AnswerKey.js";
 import PracticeSession from "./model/PracticeSession.js";
 import AnswerDetails from "./model/AnswerDetails.js";
 
-import AnswerKey from "../model/AnswerKey.js";
-
 dotenv.config();
 const app = express();
 
@@ -70,6 +68,45 @@ fs.createReadStream("../dataset/answer_key.csv")
       console.error("Error seeding job_skills data:", error);
     }
   });
+
+const readCSVFile = async (filePath) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => {
+        if (data.id) {
+          data.id = parseInt(data.id);
+          data.company_image = data.company_image || null;
+          results.push(data);
+        } else {
+          console.log("Skipping row due to missing id:", data);
+        }
+      })
+      .on("end", () => {
+        resolve(results);
+      })
+      .on("error", (err) => reject(err));
+  });
+};
+
+const seedData = async () => {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+
+    const companies = await readCSVFile("../dataset/companies.csv");
+    await Companies.bulkCreate(companies, {
+      validate: true,
+      ignoreDuplicates: true,
+    });
+    console.log("Data has been inserted successfully.");
+  } catch (error) {
+    console.error("Error during data seeding:", error);
+  }
+};
+
+seedData();
 
 
 
