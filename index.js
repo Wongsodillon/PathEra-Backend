e4ae1979bbcae5d90a1c95f90a4f4bea5c34d47e
albrtleonardi <leoneeroooo@gmail.com>
2024-09-06 21:03:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,20 +15,7 @@ const app = express();
 await db.authenticate();
 await db.sync();
 
-// Set up CORS to allow multiple origins
-const allowedOrigins = ["http://localhost:5173", "http://pathera.vercel.app"];
-
-app.use(cors({
-  credentials: true,
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-}));
-
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(Route);
