1918375ce3fcd5de06231707b87f8e1f51153074
albrtleonardi <leoneeroooo@gmail.com>
2024-09-06 15:11:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ await db.sync();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(Route);
 
 const readCSVFile = (filePath) => {
